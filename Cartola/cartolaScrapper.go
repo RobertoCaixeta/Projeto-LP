e2695f60d9fe79a015ae7f6a87f2784a7b97d363
zipudhe <lucasramalho29@gmail.com>
2022-05-02 00:04:03 -0300

    :sparkles: finishing api fetcher

--- Cartola/cartolaScrapper.go
+++ Cartola/cartolaScrapper.go
@@ -8,80 +8,107 @@ import (
 	"net/http"
 )
 
-type cartolaRes struct {
-	clubes   *clube
-	posicoes *posicao
-	status   *status
-	altetas  *atleta
+type Posicao struct {
+	id         int
+	nome       string
+	abreviacao string
 }
 
-type clube struct {
-	ArrayValue []int
+type Clube struct {
+	id            int
+	Nome          string
+	abreviacao    string
+	nome_fantasia string
 }
 
-type posicao struct {
-	ArrayValue []int
+type Atleta struct {
+	RodadaID         int
+	ClubeID          int
+	Posicao          string
+	PontosNum        float64
+	PrecoNum         float64
+	VariacaoNum      float64
+	MediaNum         float64
+	JogosNum         int
+	Apelido          string
+	ApelidoAbreviado string
+	Nome             string
 }
 
-type status struct {
-	ArrayValue []int
+type Response struct {
+	Atletas []struct {
+		Scout               struct{}    `json:"scout"`
+		AtletaID            int         `json:"atleta_id"`
+		RodadaID            int         `json:"rodada_id"`
+		ClubeID             int         `json:"clube_id"`
+		PosicaoID           int         `json:"posicao_id"`
+		StatusID            int         `json:"status_id"`
+		PontosNum           float64     `json:"pontos_num"`
+		PrecoNum            float64     `json:"preco_num"`
+		VariacaoNum         float64     `json:"variacao_num"`
+		MediaNum            float64     `json:"media_num"`
+		JogosNum            int         `json:"jogos_num"`
+		MinimoParaValorizar interface{} `json:"minimo_para_valorizar"`
+		Slug                string      `json:"slug"`
+		Apelido             string      `json:"apelido"`
+		ApelidoAbreviado    string      `json:"apelido_abreviado"`
+		Nome                string      `json:"nome"`
+		Foto                string      `json:"foto"`
+	} `json:"atletas"`
 }
 
-type atleta struct {
-	ArrayValue []int
-}
-
-func getCartolaData() []byte {
+func getCartolaData() Response {
 	res, err := http.Get("https://api.cartola.globo.com/atletas/mercado")
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	defer res.Body.Close()
+
 	// read data
-	data, err := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(res.Body)
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	jsonData, err := json.Marshal(data)
-
-	if err != nil {
-		fmt.Println("Failed to transform to json")
+	var data Response
+	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Println("Coun't unmarshal json")
 	}
 
-	return jsonData
+	return data
+}
+
+func PrettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
 }
 
 func main() {
 	// baseUrl := "https://api.cartola.globo.com/"
 
 	data := getCartolaData()
-	fmt.Printf("Json data: %s\n", data)
+
+	var atletas []Atleta
+	// var clubes []Clube
+
+	for _, atleta := range data.Atletas {
+		var player Atleta
+		player.Apelido = atleta.Apelido
+		player.JogosNum = atleta.JogosNum
+		player.ApelidoAbreviado = atleta.ApelidoAbreviado
+		player.MediaNum = atleta.MediaNum
+
+		atletas = append(atletas, player)
+	}
+
+	for _, atleta := range atletas {
+		fmt.Println("Apelido: ", atleta.Apelido)
+		fmt.Println("Qtd Jogos: ", atleta.JogosNum)
+		fmt.Println("Apeliado Abreviado: ", atleta.ApelidoAbreviado)
+		fmt.Println("Media de pontos: ", atleta.MediaNum)
+		fmt.Println()
+	}
 }
-
-// func main() {
-// 	c := colly.NewCollector(
-// 		colly.AllowedDomains("https://cartola.globo.com"),
-// 		colly.AllowURLRevisit(),
-// 	)
-
-// 	c.OnRequest(func(r *colly.Request) {
-// 		fmt.Println("Fazendo requisição para: ", r.URL)
-// 	})
-
-// 	c.OnError(func(r *colly.Response, err error) {
-// 		fmt.Println("Algo deu errado menorzada: ", err)
-// 	})
-
-// 	c.OnResponse(func(r *colly.Response) {
-// 		fmt.Println("Visitado", r.Request.URL)
-// 	})
-
-// 	c.OnHTML("div[id]", func(d *colly.HTMLElement) {
-// 		fmt.Println("Nome do Jogador:", d.Attr("id"))
-// 	})
-
-// 	c.Visit("https://cartola.globo.com/#!/time")
-// }
